a8e565fd5a5b195d15a5cf9f04cebdb27d22094f
ethicnology <ethicnology@pm.me>
2025-06-21 21:18:47 -0400

    feat: LiquidAddressParser

---
+++ lib/parsers/liquid_address_parser.dart
@@ -0,0 +1,25 @@
+import 'package:lwk/lwk.dart' as lwk;
+import 'package:satoshifier/network.dart';
+import 'package:satoshifier/satoshifier.dart' show Satoshifier;
+
+class LiquidAddressParser {
+  static Future<Satoshifier> parse(String data) async {
+    for (var lwkNetwork in lwk.Network.values) {
+      try {
+        await lwk.Address.validate(addressString: data);
+        final network = Network.fromLwkNetwork(lwkNetwork);
+        return Satoshifier.liquidAddress(address: data, network: network);
+      } catch (_) {}
+    }
+
+    throw 'Invalid liquid address: $data';
+  }
+
+  static Future<Satoshifier?> tryParse(String data) async {
+    try {
+      return await parse(data);
+    } catch (_) {
+      return null;
+    }
+  }
+}
