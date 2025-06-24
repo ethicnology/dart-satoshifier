e496872ffa5051f32dbbe1bea228d64042e8ef7e
ethicnology <ethicnology@pm.me>
2025-06-24 11:13:37 -0400

    test: add integration tests for all parsers to optimize execution

---
+++ example/integration_test/_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'bip21_test.dart' as bip21;
+import 'bitcoin_addresses_test.dart' as bitcoin;
+import 'bolt11_test.dart' as bolt11;
+import 'descriptor_test.dart' as descriptor;
+import 'liquid_addresses_test.dart' as liquid;
+import 'psbt_test.dart' as psbt;
+import 'satoshifier_test.dart' as satoshifier;
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('Integration Tests', () {
+    bip21.main();
+    bitcoin.main();
+    bolt11.main();
+    descriptor.main();
+    liquid.main();
+    psbt.main();
+    satoshifier.main();
+  });
+}
