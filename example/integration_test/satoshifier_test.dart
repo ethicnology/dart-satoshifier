39adffc73742c1b2e526ab06def6214b94a915ca
ethicnology <ethicnology@pm.me>
2025-06-24 11:13:17 -0400

    test: add integration tests for Satoshifier parsing

---
+++ example/integration_test/satoshifier_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:satoshifier/satoshifier.dart';
+
+void main() {
+  group('Satoshifier', () {
+    test('throws for empty string', () async {
+      expect(() => Satoshifier.parse(''), throwsA(isA<String>()));
+    });
+
+    test('throws for whitespace only', () async {
+      expect(() => Satoshifier.parse('   '), throwsA(isA<String>()));
+    });
+
+    test('throws for invalid input', () async {
+      expect(() => Satoshifier.parse('invalid'), throwsA(isA<String>()));
+    });
+  });
+}
