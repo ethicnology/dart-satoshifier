c130afed11b35ea46a9f78db949632455c6a47ed
ethicnology <ethicnology@pm.me>
2025-06-24 11:11:35 -0400

    fix: Bip21 Parser for non supported scheme

--- lib/parsers/bip21_parser.dart
+++ lib/parsers/bip21_parser.dart
@@ -3,51 +3,36 @@ import 'package:satoshifier/satoshifier.dart';
 import 'package:satoshifier/utils.dart';
 
 class Bip21Parser {
-  static const _supportedSchemes = [
-    'bitcoin',
-    'liquid',
-    'liquidnetwork',
-    'liquidtestnet',
-  ];
-
   static Future<Satoshifier> parse(String data) async {
-    for (final scheme in _supportedSchemes) {
-      try {
-        bip21.decode(data);
-      } catch (_) {
-        continue;
-      }
+    final uri = bip21.decode(data);
 
-      final uri = bip21.decode(data);
-      switch (scheme) {
-        case 'bitcoin':
-          await BitcoinAddressParser.parse(uri.address);
-        case 'liquid':
-          await LiquidAddressParser.parse(uri.address);
-        case 'liquidnetwork':
-          await LiquidAddressParser.parse(uri.address);
-        case 'liquidtestnet':
-          await LiquidAddressParser.parse(uri.address);
-        default:
-          throw 'Unhandled scheme: $scheme';
-      }
-
-      final amount = uri.amount;
-      final sats = amount != null ? Utils.btcToSats(amount.toString()) : 0;
-
-      return Satoshifier.bip21(
-        scheme: scheme,
-        address: uri.address,
-        uri: uri.toString(),
-        label: uri.label ?? '',
-        message: uri.message ?? '',
-        sats: sats,
-        lightning: uri.options['lightning'] as String? ?? '',
-        pj: uri.options['pj'] as String? ?? '',
-        pjos: uri.options['pjos'] as String? ?? '',
-      );
+    switch (uri.scheme) {
+      case 'bitcoin':
+        await BitcoinAddressParser.parse(uri.address);
+      case 'liquid':
+        await LiquidAddressParser.parse(uri.address);
+      case 'liquidnetwork':
+        await LiquidAddressParser.parse(uri.address);
+      case 'liquidtestnet':
+        await LiquidAddressParser.parse(uri.address);
+      default:
+        print('Unhandled scheme: ${uri.scheme} ${uri.address} not verified');
     }
-    throw 'Invalid BIP21 URI';
+
+    final amount = uri.amount;
+    final sats = amount != null ? Utils.btcToSats(amount.toString()) : 0;
+
+    return Satoshifier.bip21(
+      scheme: uri.scheme,
+      address: uri.address,
+      uri: uri.toString(),
+      label: uri.label ?? '',
+      message: uri.message ?? '',
+      sats: sats,
+      lightning: uri.options['lightning'] as String? ?? '',
+      pj: uri.options['pj'] as String? ?? '',
+      pjos: uri.options['pjos'] as String? ?? '',
+    );
   }
 
   static Future<Satoshifier?> tryParse(String data) async {
