98b70f116af089ffbecd5252ffaac614a3b126b9
ethicnology <ethicnology@pm.me>
2025-06-26 13:33:04 -0400

    feat: watch-only can access ExtendedPubkey if the descriptor is compatible

--- lib/extended_pubkey.dart
+++ lib/extended_pubkey.dart
@@ -34,4 +34,12 @@ class ExtendedPubkey {
       network: network,
     );
   }
+
+  static ExtendedPubkey? tryParse(String string) {
+    try {
+      return parse(string);
+    } catch (e) {
+      return null;
+    }
+  }
 }
--- lib/watch_only_extension.dart
+++ lib/watch_only_extension.dart
@@ -1,3 +1,10 @@
 import 'package:satoshifier/satoshifier.dart';
 
-extension WatchOnlyExtension on WatchOnly {}
+extension WatchOnlyExtension on WatchOnly {
+  String get masterFingerprint => descriptor.fingerprint;
+
+  ExtendedPubkey? get extendedPubkey =>
+      ExtendedPubkey.tryParse(descriptor.pubkey);
+
+  String get pubkeyFingerprint => extendedPubkey?.fingerprint ?? '';
+}
