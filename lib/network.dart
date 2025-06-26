d79d900b214d348cfb546a3ca7c3f16c58a3ca17
ethicnology <ethicnology@pm.me>
2025-06-26 09:06:37 -0400

    feat: enhance Network enum with Bitcoin and Liquid variants

--- lib/network.dart
+++ lib/network.dart
@@ -1,44 +1,71 @@
 import 'package:bdk_flutter/bdk_flutter.dart' as bdk;
 import 'package:lwk/lwk.dart' as lwk;
+import 'package:satoshifier/satoshifier.dart';
 
 enum Network {
-  mainnet,
-  testnet,
-  signet,
-  regtest;
+  bitcoinMainnet,
+  bitcoinTestnet,
+  bitcoinSignet,
+  bitcoinRegtest,
+  liquidMainnet,
+  liquidTestnet;
+
+  bool get isBitcoin =>
+      this == bitcoinMainnet ||
+      this == bitcoinTestnet ||
+      this == bitcoinSignet ||
+      this == bitcoinRegtest;
+  bool get isLiquid => this == liquidMainnet || this == liquidTestnet;
 
   bdk.Network get toBdk {
     switch (this) {
-      case Network.mainnet:
+      case Network.bitcoinMainnet:
         return bdk.Network.bitcoin;
-      case Network.testnet:
+      case Network.bitcoinTestnet:
         return bdk.Network.testnet;
-      case Network.signet:
+      case Network.bitcoinSignet:
         return bdk.Network.signet;
-      case Network.regtest:
+      case Network.bitcoinRegtest:
         return bdk.Network.regtest;
+      default:
+        throw 'Non bitcoin network: $this';
     }
   }
 
   static Network fromBdkNetwork(bdk.Network bdkNetwork) {
     switch (bdkNetwork) {
       case bdk.Network.bitcoin:
-        return Network.mainnet;
+        return Network.bitcoinMainnet;
       case bdk.Network.testnet:
-        return Network.testnet;
+        return Network.bitcoinTestnet;
       case bdk.Network.signet:
-        return Network.signet;
+        return Network.bitcoinSignet;
       case bdk.Network.regtest:
-        return Network.regtest;
+        return Network.bitcoinRegtest;
     }
   }
 
   static Network fromLwkNetwork(lwk.Network lwkNetwork) {
     switch (lwkNetwork) {
       case lwk.Network.mainnet:
-        return Network.mainnet;
+        return Network.liquidMainnet;
       case lwk.Network.testnet:
-        return Network.testnet;
+        return Network.liquidTestnet;
     }
+  }
+
+  static Network fromXpubType(XpubType xpubType) {
+    if (xpubType == XpubType.xpub ||
+        xpubType == XpubType.ypub ||
+        xpubType == XpubType.zpub) {
+      return Network.bitcoinMainnet;
+    }
+    if (xpubType == XpubType.tpub ||
+        xpubType == XpubType.upub ||
+        xpubType == XpubType.vpub) {
+      return Network.bitcoinTestnet;
+    }
+
+    throw 'Invalid xpub type: $xpubType';
   }
 }
