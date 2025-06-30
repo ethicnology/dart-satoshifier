34291bc9d7373a12d1645271783349dbf35a4cdc
ethicnology <ethicnology@pm.me>
2025-06-30 12:39:21 -0400

    feat: update bip32 dependency to bip32_keys and adjust imports

--- lib/utils/bip32_utils.dart
+++ lib/utils/bip32_utils.dart
@@ -1,6 +1,6 @@
 import 'dart:typed_data';
 
-import 'package:bip32/bip32.dart' as bip32;
+import 'package:bip32_keys/bip32_keys.dart';
 import 'package:bs58/bs58.dart';
 import 'package:satoshifier/enums/xpub_type.dart';
 
@@ -8,7 +8,7 @@ class Bip32Utils {
   static Uint8List fingerprint(List<int> pubkey, XpubType type) {
     final keyWithoutVersion = pubkey.sublist(4);
     final key = Uint8List.fromList([...type.version, ...keyWithoutVersion]);
-    final bip32Key = bip32.BIP32.fromBase58(
+    final bip32Key = Bip32Keys.fromBase58(
       base58.encode(key),
       bypassVersion: (type == XpubType.xpub) ? false : true,
     );
