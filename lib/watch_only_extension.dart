3316fc984275e52caa80561c765fd4297c7971c1
ethicnology <ethicnology@pm.me>
2025-06-30 13:06:02 -0400

    feat: add canGenerateValidPsbt method to WatchOnly extension

--- lib/watch_only_extension.dart
+++ lib/watch_only_extension.dart
@@ -1,3 +1,4 @@
+import 'package:convert/convert.dart';
 import 'package:satoshifier/satoshifier.dart';
 
 extension WatchOnlyExtension on WatchOnly {
@@ -7,4 +8,12 @@ extension WatchOnlyExtension on WatchOnly {
       ExtendedPubkey.tryParse(descriptor.pubkey);
 
   String get pubkeyFingerprint => extendedPubkey?.fingerprint ?? '';
+
+  bool get canGenerateValidPsbt {
+    if (masterFingerprint.isEmpty) return false;
+    if (masterFingerprint == pubkeyFingerprint) return false;
+    if (masterFingerprint.length != 8) return false;
+    if (hex.decode(masterFingerprint).length != 4) return false;
+    return true;
+  }
 }
