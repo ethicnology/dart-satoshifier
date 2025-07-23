f0c05dcbc3ef2494bf17c9f7e4474aeb03a55d79
ethicnology <ethicnology@pm.me>
2025-07-23 14:14:53 -0400

    test: add new test values for Bitcoin address parsing

--- example/integration_test/bitcoin_addresses_test.dart
+++ example/integration_test/bitcoin_addresses_test.dart
@@ -6,6 +6,11 @@ void main() {
     TestValue.mainnetP2PKH,
     TestValue.mainnetP2SH,
     TestValue.mainnetBech32,
+    // New test values
+    TestValue.bitcoinSegwitUppercase,
+    TestValue.bitcoinSegwitLowercase,
+    TestValue.bitcoinLegacy,
+    TestValue.bitcoinCompatible,
   ];
   final testnetAddresses = [
     TestValue.testnetP2PKH,
@@ -14,8 +19,8 @@ void main() {
   ];
 
   group('Bitcoin Addresses', () {
-    test('parses all valid addresses', () async {
-      for (final address in [...mainnetAddresses, ...testnetAddresses]) {
+    for (final address in [...mainnetAddresses, ...testnetAddresses]) {
+      test('parses $address', () async {
         final result = await BitcoinAddressParser.parse(address);
         expect(result, isA<BitcoinAddress>());
         expect((result as BitcoinAddress).address, address);
@@ -24,7 +29,7 @@ void main() {
         } else if (testnetAddresses.contains(address)) {
           expect((result).network, Network.bitcoinTestnet);
         }
-      }
-    });
+      });
+    }
   });
 }
