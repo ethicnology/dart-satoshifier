2e88900de282ff2f39ff9c634c63a98203d52bb8
ethicnology <ethicnology@pm.me>
2025-07-24 08:40:56 -0400

    fix: init stuck

--- lib/setup.dart
+++ lib/setup.dart
@@ -2,15 +2,15 @@ import 'package:boltz/boltz.dart' as boltz;
 import 'package:flutter/foundation.dart';
 import 'package:lwk/lwk.dart' as lwk;
 
-final Future<bool> isSatoshifierInit = LibSatoshifier.init();
+bool isSatoshifierInit = false;
 
 class LibSatoshifier {
   static Future<bool> init() async {
-    if (!await isSatoshifierInit) return true;
+    if (isSatoshifierInit) return true;
 
     try {
-      await boltz.LibBoltz.init();
-      await lwk.LibLwk.init();
+      await Future.wait([boltz.LibBoltz.init(), lwk.LibLwk.init()]);
+      isSatoshifierInit = true;
       return true;
     } catch (e) {
       debugPrint('FATAL: Failed to initialize native libraries: $e');
